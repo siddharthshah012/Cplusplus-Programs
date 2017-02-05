1019d4824150e34e275c4702185f1ee6c5dd9724
siddharthshah012 <sshah8@mail.csuchico.edu>
2017-02-05 04:07:12 -0800

    Xor Implemented/ conditions to be met

--- Using XOR/usingXOR.cpp
+++ Using XOR/usingXOR.cpp
@@ -14,78 +14,67 @@ using namespace std;
 
 int main() {
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
-	// unsigned int a,b;
-	// cin>>a>>b;
-	// int mat [b][a];
+	unsigned int a,b;
+	cin>>a>>b;
+	int mat [b][a];
 	
-	// for (int i =0; i< b-1;i++)
-	// {
-		// for (int j=0; j < a;j++)
-		// {
-			// cin >> mat[i][j];
-		// }
-	// }
-	// /*for (int i =0; i< b-1 ;i++)
-	// {
-		// for (int j=0; j < a;j++)
-		// {
-			// cout << mat[i][j];
-		// }
-	// }*/
-	// /*cout<<mat[0][0]<<endl;
-	// cout<<mat[0][1]<<endl;
-	// cout<<mat[0][2]<<endl;
-	// cout<<mat[0][3]<<endl;*/	
-	// //cout<<mat[0][3]<<endl;
-	
-	// for (int i = 0; i < b -1 ; i++)
-	// {
-		// for (int j =0; j<a;j++)
-		// {
-			
-			// if (j != a-1)
-			// {
-				// /*cout<< "I th value"<< i <<endl; 
-				// cout<<"first : " << j << "  "<<mat[i][j]<<endl;
-				// cout<<"second : "<< j+1 << "  "<<mat[i][j+1]<<endl;				
-				// cout<<"row :"<< b-1<<endl;
-				// cout<<"column :"<<j<<endl;*/
-				
-				// mat[b-1][j] = mat[i][j]^ mat[i][j+1];
-				
-				// //cout<< "answer"<<mat[b-1][j]<<endl;
-			// }
-			// else if (j == a-1)
-			// {
-				// mat[b-1][j] = mat[i][j] ^ mat [i][0];
-				// //cout<<mat[b-1][j]<<endl;
-			// }
-			
-		// }
-	// }
-	
-	// for (int i =0; i< b-1 ;i++)
-	// {
-		// for (int j=0; j < a;j++)
-		// {
-			// cout << mat[b-1][j] <<" ";
-		// }
-	// }
-	int i,j,count=0;
-	for (i =1; i<= 100;i++)
+	for (int i =0; i< b-1;i++)
 	{
-		for ( j=1;j<=log(i);j++)
+		for (int j=0; j < a;j++)
 		{
-			count++;
+			cin >> mat[i][j];
 		}
 	}
-	cout<<endl<<count<<endl;
+	/*for (int i =0; i< b-1 ;i++)
+	{
+		for (int j=0; j < a;j++)
+		{
+			cout << mat[i][j];
+		}
+	}*/
+	/*cout<<mat[0][0]<<endl;
+	cout<<mat[0][1]<<endl;
+	cout<<mat[0][2]<<endl;
+	cout<<mat[0][3]<<endl;*/	
+	//cout<<mat[0][3]<<endl;
+	
+	for (int i = 0; i < b -1 ; i++)
+	{
+		for (int j =0; j<a;j++)
+		{
+			
+			if (j != a-1)
+			{
+				/*cout<< "I th value"<< i <<endl; 
+				cout<<"first : " << j << "  "<<mat[i][j]<<endl;
+				cout<<"second : "<< j+1 << "  "<<mat[i][j+1]<<endl;				
+				cout<<"row :"<< b-1<<endl;
+				cout<<"column :"<<j<<endl;*/
+				
+				mat[b-1][j] = mat[i][j]^ mat[i][j+1];
+				
+				//cout<< "answer"<<mat[b-1][j]<<endl;
+			}
+			else if (j == a-1)
+			{
+				mat[b-1][j] = mat[i][j] ^ mat [i][0];
+				//cout<<mat[b-1][j]<<endl;
+			}
+			
+		}
+	}
+	
+	for (int i =0; i< b-1 ;i++)
+	{
+		for (int j=0; j < a;j++)
+		{
+			cout << mat[b-1][j] <<" ";
+		}
+	}
 	
 	
-	/*cout<<mat[1][0]<<endl;
-	cout<<mat[1][1]<<endl;
-	cout<<mat[1][2]<<endl;
-	cout<<mat[1][3]<<endl;	*/
+	
+	
     
     return 0;
 }
